d82269ab91bb1810722af6ff1ab9e466916f1619
David Kim <daaavidkim@gmail.com>
2026-06-02 16:40:29 -0500

    Added security Obj filter

--- webcam_detector.py
+++ webcam_detector.py
@@ -1,14 +1,38 @@
 from ultralytics import YOLO
 import cv2
 
+# loads yolo model
 model = YOLO("yolo11n.pt")
 
+# since we only need necessary objects for security cam
+# create watch list with only necessary objects
+# only obj we check for
+
+SECURITY_OBJ = {
+    "person",
+    "car",
+    "truck",
+    "motorcycle"
+}
+
+OBJ_STATE = {
+    "person": False,
+    "car" : False,
+    "truck" : False,
+    "motorcycle" : False,
+}
+
+
+MIN_CON = 0.50
+
+# opens webcam
 cap = cv2.VideoCapture(0)
 
 while True:
     success, frame = cap.read()
 
     if not success:
+        print("Failing to read webcam.")
         break
 
     results = model(frame)
@@ -18,19 +42,22 @@ while True:
 
             class_id = int(box.cls[0])
             confidence = float(box.conf[0])
+            label = result.names[class_id]
 
-            print(
-                f"{result.names[class_id]} "
-                f"{confidence:.2f}"
-            )
 
+            #checks if in security_obj and that the model is confident
+            if(label in SECURITY_OBJ and confidence > MIN_CON):
+                print(f"Deteced: {label} "
+                      f"({confidence:.2f})")
+
+    #boxes
     annotated_frame = results[0].plot()
-
+    # captures everythingokay
     cv2.imshow(
-        "Object Detector",
+        "Security Camera",
         annotated_frame
     )
-
+    #esc = quit
     if cv2.waitKey(1) == 27:
         break
 

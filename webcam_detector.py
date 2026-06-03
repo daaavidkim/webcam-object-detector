caa6a815208ae36ec9ea066ce29f43866c263c14
David Kim <daaavidkim@gmail.com>
2026-06-03 18:46:50 -0500

    Added Event logging and presence tracking

--- webcam_detector.py
+++ webcam_detector.py
@@ -1,5 +1,8 @@
 from ultralytics import YOLO
 import cv2
+import os
+import csv
+from datetime import datetime
 
 # loads yolo model
 model = YOLO("yolo11n.pt")
@@ -15,15 +18,52 @@ SECURITY_OBJ = {
     "motorcycle"
 }
 
-OBJ_STATE = {
-    "person": False,
+MIN_CON = 0.50
+EXIT = 2
+#how long obj must be gone for it to be recorded **this is for frame lag
+#or in case camera doesn't catch obj for a frame
+
+
+#check folder exists
+os.makedirs("captures/entries", exist_ok=True)
+os.makedirs("captures/exits", exist_ok=True)
+os.makedirs("logs", exist_ok=True)
+csv_file = "logs/events.csv"
+
+#only want to create csv file once
+#first time (not false)
+if not os.path.exists(csv_file):
+    with open(csv_file, "w", newline="") as file:
+        write = csv.writer(file)
+        write.writerow([
+            "timestamp",
+            "event",
+            "object",
+            "duration_seconds"
+        ])
+
+# obj state
+
+OBJECT_STATE = {
+    "person" : False,
     "car" : False,
     "truck" : False,
-    "motorcycle" : False,
+    "motorcycle" : False
 }
 
+ENTRY_TIMES = { 
+    "person" : None,
+    "car" : None,
+    "truck" : None,
+    "motorcycle" : None
+}
 
-MIN_CON = 0.50
+LAST_SEEN = {
+    "person" : None,
+    "car" : None,
+    "truck" : None,
+    "motorcycle" : None
+}
 
 # opens webcam
 cap = cv2.VideoCapture(0)
@@ -37,6 +77,8 @@ while True:
 
     results = model(frame)
 
+    detected_objects = set()
+
     for result in results:
         for box in result.boxes:
 
@@ -46,9 +88,77 @@ while True:
 
 
             #checks if in security_obj and that the model is confident
-            if(label in SECURITY_OBJ and confidence > MIN_CON):
-                print(f"Deteced: {label} "
-                      f"({confidence:.2f})")
+            if(label in SECURITY_OBJ and confidence >= MIN_CON):
+                detected_objects.add(label)
+                LAST_SEEN[label] = datetime.now()
+
+    #obj entry
+
+    for obj in detected_objects:
+
+        if not OBJECT_STATE[obj]:
+            OBJECT_STATE[obj] = True
+
+            entry_time = datetime.now()
+            ENTRY_TIMES[obj] = entry_time
+
+            print(
+                f"{obj.upper()} ENTERED at "
+                f"{entry_time.strftime('%H:%M:%S')}"
+            )
+
+            filename = (
+                f"captures/entries/"
+                f"{obj}_{entry_time.strftime('%Y%m%d_%H%M%S')}.jpg"
+            )
+
+            cv2.imwrite(filename, frame)
+            with open(csv_file, "a", newline="") as file:
+                writer = csv.writer(file)
+
+                writer.writerow([
+                    entry_time,
+                    "ENTER",
+                    obj,
+                    ""
+                ])
+
+    #obj exits
+    for obj in SECURITY_OBJ:
+        if OBJECT_STATE[obj]:
+
+            time_since_seen = (datetime.now() - LAST_SEEN[obj]).total_seconds()
+
+            if time_since_seen >= EXIT:
+                exit_time = datetime.now()
+
+                duration = (exit_time - ENTRY_TIMES[obj]).total_seconds()
+
+                print(
+                    f"{obj.upper()} EXITED at "
+                    f"{exit_time.strftime('%H:%M:%S')} "
+                    f"(Present for {duration:.1f}s)"
+                )
+
+                filename = (
+                    f"captures/exits/"
+                    f"{obj}_{exit_time.strftime('%Y%m%d_%H%M%S')}.jpg"
+                )
+
+                cv2.imwrite(filename, frame)
+
+                with open(csv_file, "a", newline="") as file:
+                    writer = csv.writer(file)
+
+                    writer.writerow([
+                        exit_time,
+                        "EXIT",
+                        obj,
+                        f"{duration:.1f}"
+                    ])
+                OBJECT_STATE[obj] = False
+                ENTRY_TIMES[obj] = None
+
 
     #boxes
     annotated_frame = results[0].plot()
